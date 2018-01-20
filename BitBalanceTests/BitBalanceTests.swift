0ff95a8f1bd315fa70a8b10365167cbffb741e8b
JomiMoldes <josemiguel.moldes@gmail.com>
2018-01-20 20:24:13 -0300

    Project initialization

---
+++ BitBalanceTests/BitBalanceTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitBalanceTests.swift
+//  BitBalanceTests
+//
+//  Created by MIGUEL MOLDES on 20/1/18.
+//  Copyright © 2018 MIGUEL MOLDES. All rights reserved.
+//
+
+import XCTest
+@testable import BitBalance
+
+class BitBalanceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
